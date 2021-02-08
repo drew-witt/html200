a21e68d3353e20a41af02572dd00c2afe2b46173
drew-witt <55118872+drew-witt@users.noreply.github.com>
2021-02-08 14:04:13 -0800

    Create exercise-one.js

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,13 @@
+let faveToys = ['Buzz', 'Woody', 'Mr. Potato Head', 'Rex', 'RC'];
+
+faveToys.push('Jessie')
+
+console.log(faveToys);
+
+faveToys.splice(2, 1);
+
+console.log(faveToys)
+
+const faveToyslist = faveToys.join(', ');
+
+console.log(faveToyslist);
